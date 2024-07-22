72c16751667f1369c09197954e8b9227299250f4
iGH01gi <iGH01pr@outlook.com>
2024-07-23 06:56:05 +0900

    init 초기 서버 설정 완료(pch, 링크설정, 폴더 설정 등등)

---
+++ ServerCore/Utils/CorePch.h
@@ -0,0 +1,19 @@
+//더미나 게임서버에서 사용
+#pragma once
+
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+
+#include "Types.h"
+
+using namespace std;
+
+void HelloWorld();
+
