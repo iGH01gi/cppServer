7c798ec78522500de4e2dfc7c36bffa2895fbb6d
iGH01gi <iGH01pr@outlook.com>
2024-10-16 23:54:06 +0900

    SLIST_ENTRY와 SLIST_HEADER를 이용한 락프리 구조의 메모리풀로 수정
    기존은 lock을 잡는 방식이었음

--- GameServer/Main/GameServer.cpp
+++ GameServer/Main/GameServer.cpp
@@ -88,20 +88,49 @@ void fnc(T&& n) {
 
 }
 
-class MyClass
-{
-	
+
+// 32바이트 정렬을 위한 구조체 정의
+DECLSPEC_ALIGN(32)
+struct BigStruct {
+	char data[30]; // 30바이트 크기
+};
+
+// 16바이트 정렬을 위한 SListEntry 구조체
+DECLSPEC_ALIGN(16)
+struct SListEntry {
+	SListEntry* next; // 다음 항목을 가리키는 포인터
+};
+
+// MyClass 정의
+class MyClass {
+public:
+	SListEntry entry; // SListEntry를 멤버로 사용
+	BigStruct bigData; // 30바이트 크기의 멤버 추가
 };
 
 int main()
 {
+	MyClass myObject;
+
+	std::cout << "Size of MyClass: " << sizeof(MyClass) << std::endl;
+	//entry의 크기
+	std::cout << "Size of entry: " << sizeof(myObject.entry) << std::endl;
+	//bigData의 크기
+	std::cout << "Size of bigData: " << sizeof(myObject.bigData) << std::endl;
+	std::cout << "Address of entry: " << &myObject.entry << std::endl;
+	std::cout << "Address of bigData: " << &myObject.bigData << std::endl;
+
+	// 16바이트 및 30바이트 정렬 확인
+	std::cout << "Address of entry is " << ((reinterpret_cast<uintptr_t>(&myObject.entry) % 16 == 0) ? "aligned to 16 bytes." : "NOT aligned to 16 bytes.") << std::endl;
+	std::cout << "Address of bigData is " << ((reinterpret_cast<uintptr_t>(&myObject.bigData) % 32 == 0) ? "aligned to 32 bytes." : "NOT aligned to 32 bytes.") << std::endl;
+
 	//내 pc의 page크기를 알아보기 위한 코드를 실행
-		//SYSTEM_INFO si;
+		/*//SYSTEM_INFO si;
 	SYSTEM_INFO si;
 	GetSystemInfo(&si);
 
 	// 페이지 크기 출력
-	std::cout << "Page size: " << si.dwPageSize << " bytes" << std::endl;
+	std::cout << "Page size: " << si.dwPageSize << " bytes" << std::endl;*/
 
 	/*int n = 10;
 
--- ServerCore/Memory/MemoryPool.h
+++ ServerCore/Memory/MemoryPool.h
@@ -1,10 +1,16 @@
 #pragma once
 
+enum
+{
+	SLIST_ALIGNMENT = 16
+};
+
 /*-----------------
 	MemoryHeader
 ------------------*/
 
-struct MemoryHeader
+DECLSPEC_ALIGN(SLIST_ALIGNMENT)
+struct MemoryHeader : public SLIST_ENTRY
 {
 	// [MemoryHeader][Data]
 	MemoryHeader(int32 size) : allocSize(size) { }
@@ -21,7 +27,7 @@ struct MemoryHeader
 		return header;
 	}
 
-	int32 allocSize; //헤더크기+데이터크기 합한 크기...
+	int32 allocSize;
 	// TODO : 필요한 추가 정보
 };
 
@@ -29,20 +35,19 @@ struct MemoryHeader
 	MemoryPool
 ------------------*/
 
+DECLSPEC_ALIGN(SLIST_ALIGNMENT)
 class MemoryPool
 {
 public:
 	MemoryPool(int32 allocSize);
 	~MemoryPool();
 
-	void Push(MemoryHeader* ptr);
+	void			Push(MemoryHeader* ptr);
 	MemoryHeader* Pop();
 
 private:
-	int32 _allocSize = 0;
-	atomic<int32> _allocCount = 0;
-
-	USE_LOCK;
-	queue<MemoryHeader*> _queue;
+	SLIST_HEADER	_header;
+	int32			_allocSize = 0;
+	atomic<int32>	_allocCount = 0;
 };
 
